8c4c56a7a79e79c12a900191fb98d426d0c1e4cc
badhon223466 <mdbadhon7734@gmail.com>
2026-05-25 13:54:51 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/plcsimulation.pro.bd',   // 🔥 MUST for custom domain
+    base: '/Plc-simulator/',   // 🔥 MUST MATCH YOUR REPO NAME
+    plugins: [react(), tailwindcss()],
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
     define: {
       'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
-    }
+      },
+    },
   };
 });
